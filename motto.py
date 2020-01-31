e9998e6a081cdc5008919ada0896d7e7e01511b8
李敬1 <lijing1@lijingdeMacBook-Pro.local>
2020-01-31 15:40:45 +0800

    Initial commit

---
+++ motto.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+print ('go big or go home')
+# 输出gg
+str1 = 'gg'
+print (str1)
+# 输出hh
+str2 = 'hh'
+print (str2)
---
+++ test.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# -*- coding utf-8 -*-
+# 开发团队：明日边缘
+# 开发人员：lijing1
+# 开发时间：2020/1/28 上午11:15
+# 开发名称：test.py
+# 开发工具：PyCharm 
+import os
+os.system()
